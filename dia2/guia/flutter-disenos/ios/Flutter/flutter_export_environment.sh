ea088545f30d7ac1fde6e9712246a58c5cdd00c5
vidapogosoft <vidapogosoft@hotmail.com>
2021-11-06 12:40:18 -0500

    dia 2
    final taller de diseño de apps

--- dia2/guia/flutter-disenos/ios/Flutter/flutter_export_environment.sh
+++ dia2/guia/flutter-disenos/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Cursos\2020\SIPECOM\Flutter092020\TalleresDia2\flutter-disenos"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\vidapogosoft\cursos\2021\SIPECOM\Flutter102021\code\dia2\guia\flutter-disenos"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"

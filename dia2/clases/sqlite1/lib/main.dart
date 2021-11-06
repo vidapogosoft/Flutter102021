8877a062f1f6fb38628f2c68746e5868f21427ff
vidapogosoft <vidapogosoft@hotmail.com>
2021-11-06 15:27:44 -0500

    taller dia 2 sqflite
    desarrollo completo del ejercicio de base de datos inetrna sqlite

--- dia2/clases/sqlite1/lib/main.dart
+++ dia2/clases/sqlite1/lib/main.dart
@@ -2,7 +2,8 @@
 
 import 'package:flutter/material.dart';
 
-//import 'package:sqlite1/model/client_model.dart';
+import 'package:sqlite1/model/client_model.dart';
+import 'package:sqlite1/add_editclient.dart';
 import 'package:sqlite1/db/database.dart';
 
 void main() => runApp(new MyApp());
@@ -35,30 +36,133 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Sqlite"),
-        actions: <Widget>[
-          RaisedButton(
-            color: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          title: Text("Flutter Sqlite"),
+          actions: <Widget>[
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              onPressed: () {
+                ClientDatabaseProvider.db.deleteAllClient();
+                setState(() {});
+              },
+              child: Text('Delete All',
+                  style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.0,
+                      color: Colors.white)),
+            )
+          ],
+        ),
+        body: FutureBuilder<List<Client>>(
+          //we call the method, which is in the folder db file database.dart
+          future: ClientDatabaseProvider.db.getAllClients(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Client>> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                physics: BouncingScrollPhysics(),
+                //Count all records
+                itemCount: snapshot.data.length,
+                //all the records that are in the client table are passed to an item Client item = snapshot.data [index];
+                itemBuilder: (BuildContext context, int index) {
+                  Client item = snapshot.data[index];
+                  //delete one register for id
+                  return Dismissible(
+                    key: UniqueKey(),
+                    background: Container(color: Colors.red),
+                    onDismissed: (diretion) {
+                      ClientDatabaseProvider.db.deleteClientWithId(item.id);
+                    },
+                    //Now we paint the list with all the records, which will have a number, name, phone
+                    child: ListTile(
+                      title: Text(item.name),
+                      subtitle: Text(item.phone),
+                      leading: CircleAvatar(child: Text(item.id.toString())),
+                      //If we press one of the cards, it takes us to the page to edit, with the data onTap:
+                      //This method is in the file add_editclient.dart
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => AddEditClient(
+                                  true,
+                                  //Here is the record that we want to edit
+                                  client: item,
+                                )));
+                      },
+                    ),
+                  );
+                },
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButton:
+            Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+          FloatingActionButton(
+            child: Icon(Icons.add),
             onPressed: () {
-              ClientDatabaseProvider.db.deleteAllClient();
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => AddEditClient(false)));
+            },
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.refresh),
+            onPressed: () {
+              //ClientDatabaseProvider.db.getAllClients();
+              _CrearLista();
               setState(() {});
             },
-            child: Text('Delete All',
-                style: new TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14.0,
-                    color: Colors.white)),
           )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.refresh),
-        onPressed: () {
-          ClientDatabaseProvider.db.getAllClients();
-          setState(() {});
-        },
-      ),
+        ]));
+  }
+
+  Widget _CrearLista() {
+    FutureBuilder<List<Client>>(
+      //we call the method, which is in the folder db file database.dart
+      future: ClientDatabaseProvider.db.getAllClients(),
+      builder: (BuildContext context, AsyncSnapshot<List<Client>> snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            physics: BouncingScrollPhysics(),
+            //Count all records
+            itemCount: snapshot.data.length,
+            //all the records that are in the client table are passed to an item Client item = snapshot.data [index];
+            itemBuilder: (BuildContext context, int index) {
+              Client item = snapshot.data[index];
+              //delete one register for id
+              return Dismissible(
+                key: UniqueKey(),
+                background: Container(color: Colors.red),
+                onDismissed: (diretion) {
+                  ClientDatabaseProvider.db.deleteClientWithId(item.id);
+                },
+                //Now we paint the list with all the records, which will have a number, name, phone
+                child: ListTile(
+                  title: Text(item.name),
+                  subtitle: Text(item.phone),
+                  leading: CircleAvatar(child: Text(item.id.toString())),
+                  //If we press one of the cards, it takes us to the page to edit, with the data onTap:
+                  //This method is in the file add_editclient.dart
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => AddEditClient(
+                              true,
+                              //Here is the record that we want to edit
+                              client: item,
+                            )));
+                  },
+                ),
+              );
+            },
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }

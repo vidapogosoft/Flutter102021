646cf2d90de3adf4f0e0d1964d1b428e29f6b802
vidapogosoft <vidapogosoft@hotmail.com>
2021-11-13 12:20:38 -0500

    taller dia 3 - camara tomar con el dispositivo y de la galleria

---
+++ dia3/clases/foto_app/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'package:foto_app/pages/fotos.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo Foto App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Demo de fotos en flutter:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Ir a Fotos",
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => Fotos()));
+        },
+        child: Icon(Icons.photo),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}

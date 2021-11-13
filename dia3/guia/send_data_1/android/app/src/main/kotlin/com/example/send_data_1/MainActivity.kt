ae27ac74fb33ee470873a6cc7c311fd1b4a7af7d
vidapogosoft <vidapogosoft@hotmail.com>
2021-11-13 09:18:31 -0500

    GUIAS DE EJEMPLO DIA3
    material para el participante

---
+++ dia3/guia/send_data_1/android/app/src/main/kotlin/com/example/send_data_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.send_data_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dia3/guia/validate_form_1/android/app/src/main/kotlin/com/example/validate_form_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.validate_form_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
